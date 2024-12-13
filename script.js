e1c833a8801671cad58a56f7f14d71fed05a70c5
andrewtargino <108430150+andrewtargino@users.noreply.github.com>
2024-12-13 18:14:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 console.log("oi")
-alert("BEM VINDO")
+//alert("BEM VINDO")
 const nome="idioma";
-const botao=document.getElementById("btn").textContent
+// precisa tirar o .textContent pra usar a funçao abaixo;
+const botao=document.getElementById("btn")
 console.log(botao)
-const linguagens=document.querySelector("linguagens")
-const idiomaescolhido=document.querySelectorAll("PT")
-const movies=document.getElementByIs("movies");
+const linguagens=document.querySelector(".linguagens")
+const idiomaescolhido=document.querySelectorAll(".PT")
+
+//tudo que for mudado precisa ter um get ElementbYiD E UM CONTEUDO EM PT E EM EN
+const movies=document.getElementById("movies");
+const movies1=document.getElementById("movies1");
+const movies2=document.getElementById("movies2");
 
 const translations={
     en:{
-        texto:"movies and series"
+        texto:"Unlimeted movies,",
+        texto2: "TV shows,",
+        texto3: "and more"
     },
     pt:{
         texto:"filmes e séries"
     }
 }
-botao.addeventlistener('click',
+//renomear o addEventListener
+botao.addEventListener('click',
     ()=>{
       linguagens.classList.toggle("mostrar")  
     }
 ) 
+//renomear o addEventListener; 
 idiomaescolhido.forEach((idioma) =>{
-    idioma.addeventlistener("click", () => {
-        const lang= idioma.getAttribute("data-lang");
+    idioma.addEventListener("click", () => {
+        const lang=idioma.getAttribute("data-lang");
+      
+        botao.textContent = idioma.textContent;
+
+
+        //textp que vai ser mudado. 
         movies.textContent=translations[lang].texto
+        // mais conteudo que vai ser mudado ...
+        movies1.textContent=translations[lang].texto2
+        movies2.textContent=translations[lang].texto3
         linguagens.classList.remove("mostrar")
     })
-}
-    
-    
+}   
 )   
+

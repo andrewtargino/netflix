ed89243e0cd66e92d5b3adfa7f478b787cdba480
andrewtargino <108430150+andrewtargino@users.noreply.github.com>
2024-12-13 07:29:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ alert("BEM VINDO")
 const nome="idioma";
 const botao=document.getElementById("btn").textContent
 console.log(botao)
+const linguagens=document.querySelector("linguagens")
+const idiomaescolhido=document.querySelectorAll("PT")
+const movies=document.getElementByIs("movies");
+
+const translations={
+    en:{
+        texto:"movies and series"
+    },
+    pt:{
+        texto:"filmes e séries"
+    }
+}
+botao.addeventlistener('click',
+    ()=>{
+      linguagens.classList.toggle("mostrar")  
+    }
+) 
+idiomaescolhido.forEach((idioma) =>{
+    idioma.addeventlistener("click", () => {
+        const lang= idioma.getAttribute("data-lang");
+        movies.textContent=translations[lang].texto
+        linguagens.classList.remove("mostrar")
+    })
+}
+    
+    
+)   
